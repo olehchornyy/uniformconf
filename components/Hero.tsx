e3a5307b8b9caffde4747cb46e3cbe378ce96cc1
Oleh Chornyy <Oleh.Chornyy@solarwinds.com>
2022-04-05 12:34:24 +0300

    test commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ export function Hero({
         <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
           <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left min-h-500">
             <p className="uppercase tracking-loose w-full">
-              This is Uniform demo
+              This is Uniform demo test from <b>Oleh Chornyy</b>
             </p>
             <h1
               className="my-4 text-5xl font-bold leading-tight"
